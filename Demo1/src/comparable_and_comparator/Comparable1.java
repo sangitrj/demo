55ac9f13d9dd3480d3ede529a36571aacf6d77b3
ABCD <ABCD@ABCD-PC>
2017-11-19 20:29:31 +0530

    Comparable demo

---
+++ Demo1/src/comparable_and_comparator/Comparable1.java
@@ -0,0 +1,17 @@
+package comparable_and_comparator;
+
+import java.util.Arrays;
+
+public class Comparable1 {
+	public static void main(String[] args) {
+		Employee[] empArr = new Employee[4];
+		empArr[0] = new Employee(10, "Mikey", 25, 10000);
+		empArr[1] = new Employee(20, "Arun", 29, 20000);
+		empArr[2] = new Employee(1, "Lisa", 35, 5000);
+		empArr[3] = new Employee(1, "Pankaj", 32, 3400);
+		Arrays.sort(empArr);
+		for(Employee obj : empArr)
+			System.out.println(obj);
+		//System.out.println(Arrays.toString(empArr));
+	}
+}
---
+++ Demo1/src/comparable_and_comparator/Employee.java
@@ -0,0 +1,42 @@
+package comparable_and_comparator;
+
+public class Employee implements Comparable<Employee>{
+
+    private int id;
+    private String name;
+    private int age;
+    private long salary;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public long getSalary() {
+        return salary;
+    }
+
+    public Employee(int id, String name, int age, int salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString(){
+    	return id+" : "+name+" : "+age+" : "+salary;
+    }
+	@Override
+	public int compareTo(Employee o) {
+		return this.id - o.id;
+	}
+
+}
